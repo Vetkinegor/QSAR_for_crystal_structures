a40fc761ec59ecbf1deb4340a82480ca8f35523f
Vetkinegor <40809622+Vetkinegor@users.noreply.github.com>
2018-07-04 02:54:21 +0300

    Create PredictBandGap.m

---
+++ PredictBandGap.m
@@ -0,0 +1,43 @@
+
+%   a - input data.
+%   bandgapDFT3 - target data.
+
+x = a';
+t = bandgapDFT3';
+trainFcn = 'trainlm';  % Levenberg-Marquardt backpropagation.
+
+% Create a Fitting Network
+hiddenLayerSize = 10;
+net = fitnet(hiddenLayerSize,trainFcn);
+net.input.processFcns = {'removeconstantrows','mapminmax'};
+net.output.processFcns = {'removeconstantrows','mapminmax'};
+net.divideFcn = 'dividerand';  % Divide data randomly
+net.divideMode = 'sample';  % Divide up every sample
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
+net.performFcn = 'mse';  % Mean Squared Error
+net.plotFcns = {'plotperform','plottrainstate','ploterrhist', ...
+    'plotregression', 'plotfit'};
+[net,tr] = train(net,x,t);
+y = net(x);
+e = gsubtract(t,y);
+performance = perform(net,t,y)
+trainTargets = t .* tr.trainMask{1};
+valTargets = t .* tr.valMask{1};
+testTargets = t .* tr.testMask{1};
+trainPerformance = perform(net,trainTargets,y)
+valPerformance = perform(net,valTargets,y)
+testPerformance = perform(net,testTargets,y)
+view(net)
+if (false)
+    genFunction(net,'myNeuralNetworkFunction');
+    y = myNeuralNetworkFunction(x);
+end
+if (false)
+    genFunction(net,'myNeuralNetworkFunction','MatrixOnly','yes');
+    y = myNeuralNetworkFunction(x);
+end
+if (false)
+    gensim(net);
+end
